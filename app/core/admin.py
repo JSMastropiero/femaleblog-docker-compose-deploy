24994bb7aad8433f28c1c015cff24bf42693c858
andres <andred.mopez@gmail.com>
2023-03-12 18:28:36 -0300

    admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 from .models import Article, Comment, File, TypeOfFile, Profile
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = [
+        'title',
+        'tags',
+    ]
+    list_display = ['title','id', 'created', 'modified', 'is_active', 'user', 'count' ]
+    #autocomplete_fields = ['charger']
 
 
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Comment)
 admin.site.register(File)
 admin.site.register(TypeOfFile)
 admin.site.register(Profile)
+
